80241a648c80d4baf9530170396d7f51434d6777
Melissa Malpica <Malpicadz@gmail.com>
2015-06-10 17:55:01 -0400

    Setting up app to listen for incoming request from my localhost / port.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('YEAH!, Science');
+  res.type('text/plain');// My data type
+  res.send('YEAH!, Science'); // My Messege
+});
+
+var server = app.listen(8080, function() {
+	console.log ('Listening on port 8080');
 });
