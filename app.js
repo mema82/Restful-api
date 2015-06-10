710a6db0d25e53baa1e98ce31ef74a1a1dbd4f3f
Melissa Malpica <Malpicadz@gmail.com>
2015-06-10 18:41:20 -0400

    TDD and Debugging on route requests.

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.type('text/plain');// My data type
-  res.send('quotes.js'); // My Test Messege
-  res.json(quotes); //Testing to send actual json file
+  res.send('YEAH! Science'); // My Test Messege
+  res.json(quotes.js); //Testing to send actual json file
 });
 
+/*
 //Adding new route
 app.get('/quote/random', function(req, res) { // new '/quote/random' - route added
   var id = Math.floor(Math.random() * quotes.length);//ability to retrirve random quote
@@ -15,6 +16,16 @@ app.get('/quote/random', function(req, res) { // new '/quote/random' - route add
 });
 
 
+//Adding ability to grab single quote
+app.get('/quote/:id', function(req, res) { //Adding '/quote/:id' -route
+  if(quotes.length <= req.params.id || req.params.id < 0) { //Id available by adding ":param"
+    res.statusCode = 404;
+    return res.send('Error 404: No quote found');
+  }  
+var q = quotes[req.params.id];
+  res.json(q);
+});
+*/
 var server = app.listen(8080, function() {
 	console.log ('Listening on port 8080');
 });
