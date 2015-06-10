c434af329a746f98ec73191a0b9abf3d9e858692
Melissa Malpica <Malpicadz@gmail.com>
2015-06-10 18:14:23 -0400

    Adding new route for ability to grab random quotes.

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.type('text/plain');// My data type
-  res.send('YEAH!, Science'); // My Messege
+  res.send('quotes.js'); // My Test Messege
+  res.json(quotes); //Testing to send actual json file
 });
+
+//Adding new route
+app.get('/quote/random', function(req, res) { // new '/quote/random' - route added
+  var id = Math.floor(Math.random() * quotes.length);//ability to retrirve random quote
+  var q = quotes[id]; //grabbing quote
+  res.json(q); //returning quote
+});
+
 
 var server = app.listen(8080, function() {
 	console.log ('Listening on port 8080');
